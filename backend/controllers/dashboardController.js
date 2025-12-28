3a3a405a43887dd0c2af09f4ad0895bd3f248e76
Diksha0032 <chaurasiyadiksha11@gmail.com>
2025-12-29 02:50:54 +0530

    income,expense n dashboard api

---
+++ backend/controllers/dashboardController.js
@@ -0,0 +1,64 @@
+const Income=require("../models/Income")
+const Expense=require("../models/Expense")
+const {isValidObjectId,Types}=require("mongoose")
+
+exports.getDashboardData=async(req,res)=>{
+  try{
+    const userId=req.user.id;
+    const userObjectId=new Types.ObjectId(String(userId));
+
+    const totalIncome=await Income.aggregate([
+      {$match:{userId:userObjectId}},
+      {$group:{_id:null,total:{$sum:"$amount"}}}
+    ])
+
+    console.log("totalIncome",{totalIncome,userId:isValidObjectId(userId)})
+
+     const totalExpense=await Expense.aggregate([
+      {$match:{userId:userObjectId}},
+      {$group:{_id:null,total:{$sum:"$amount"}}}
+    ])
+
+    const last30DaysIncomeTransactions=await Income.find({
+      userId,date:{$gte:Date.now()-30*24*60*60*1000}
+    }).sort({date:-1})
+
+    const incomeLast30Days=last30DaysIncomeTransactions.reduce((sum,transaction)=>sum+transaction.amount,0)
+
+    const last30DaysExpenseTransactions=await Expense.find({
+      userId,date:{$gte:Date.now()-30*24*60*60*1000}
+    }).sort({date:-1})
+
+    const expenseLast30Days=last30DaysExpenseTransactions.reduce((sum,transaction)=>sum+transaction.amount,0)
+
+     const lastTransactions=[
+    ...(await Income.find({userId}).sort({date:-1}).limit(10)).map(
+      (tsn)=>({
+        ...tsn.toObject(),
+        type:"expense",
+      })
+    ),...(await Income.find({userId}).sort({date:-1}).limit(10)).map(
+      (tsn)=>({
+        ...tsn.toObject(),
+        type:"expense",
+      })
+    ),
+  ].sort((a,b)=>b.date-a.date);
+  res.json({
+    totalBalance:(totalIncome[0]?.total || 0) - (totalExpense[0]?.total || 0),
+    totalIncome:totalIncome[0]?.total || 0,
+    totalExpense:totalExpense[0]?.total || 0,
+    last30DaysExpenses:{
+      total:expenseLast30Days,
+      transactions:last30DaysExpenseTransactions,
+    },
+    last30DaysIncome:{
+      total:incomeLast30Days,
+      transactions:last30DaysIncomeTransactions,
+    },
+    recentTransactions:lastTransactions,
+  })
+  }catch{
+    res.status(500).json({message:"Server Error"})
+  }
+}
--- backend/server.js
+++ backend/server.js
@@ -5,9 +5,14 @@ const cors=require("cors");
 const path=require("path");
 const connectDB=require("./config/db");
 const authRoutes=require("./routes/authRoutes")
+const incomeRoutes=require("./routes/incomeRoutes")
+const expenseRoutes=require("./routes/expenseRoutes")
 const app=express();
+const dashboardRoutes=require("./routes/dashboardRoutes")
 
-app.use("/api/v1/auth",authRoutes)
+connectDB();
+
+app.use(express.json());
 
 app.use(
   cors({
@@ -17,9 +22,10 @@ app.use(
   })
 )
 
-app.use(express.json())
-
-connectDB();
+app.use("/api/v1/auth",authRoutes)
+app.use("/api/v1/auth/income",incomeRoutes)
+app.use("/api/v1/auth/expense",expenseRoutes)
+app.use("/api/v1/dashboard",dashboardRoutes)
 
 const PORT=process.env.PORT || 8001;
 
